22d42173bc5e294efac6973583da1f30bba37cd4
Vlad Melnyk <vlad22523@gmail.com>
2024-03-11 21:35:26 +0200

    pr2

---
+++ pr2/pr2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pr2
+{
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var task_2 = new task1();
+            task_2.start    
+
+        }
+    }
+}  
